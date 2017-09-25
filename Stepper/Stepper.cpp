97984a3be82b154aa9d1b8cac93ed7aedddb5867
raulMrello <raul.martin.rello@gmail.com>
2017-09-25 19:28:51 +0200

    Añado nuevos módulos Shifter, Stepper y PirDetector

---
+++ Stepper/Stepper.cpp
@@ -0,0 +1,116 @@
+/*
+    Copyright (c) 2017 @raulMrello
+ 
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+ 
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+ 
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+    
+    @file          Stepper.cpp
+    @purpose       Stepper motor controller
+    @version       v1.0
+    @date          Jun 2017
+    @author        @raulMrello
+*/
+
+#include "Stepper.h"
+
+//- PRIVATE -----------------------------------------------------------------------
+
+#define STEP_RESOLUTION     0.087890625f        // (360º)/4096steps
+
+const uint8_t wave_drive[]={0x08, 0x04, 0x02, 0x01, 0x08, 0x04, 0x02, 0x01};
+const uint8_t full_step[] ={0x0c, 0x06, 0x03, 0x09, 0x0c, 0x06, 0x03, 0x09};
+const uint8_t half_step[] ={0x08, 0x0c, 0x04, 0x06, 0x02, 0x03, 0x01, 0x09};
+
+
+//- IMPL. -------------------------------------------------------------------------
+
+Stepper::Stepper(uint8_t id, Stepper_mode_t mode, Logger* logger){
+    _logger = logger;
+	PRINT_LOG(_logger, "[Stepper %d]  Configurando...\r\n",id);
+
+	_id = id;
+	_steps = 0;
+    _max_deg_limit = MAX_DEG_DEFAULT;
+    _min_deg_limit = MIN_DEG_DEFAULT;
+    _degrees = 0; 
+	_clockwise = true;
+	_step = 0;
+	_sequence = full_step;
+	_mode = mode;
+	if(mode == Stepper::WAVE_DRIVE){
+		_sequence = wave_drive;
+	}
+	else if(mode == Stepper::HALF_STEP){
+		_sequence = half_step;
+	}
+	PRINT_LOG(_logger, "[Stepper %d]  Listo\r\n", _id);
+}
+
+
+uint8_t Stepper::request(uint16_t degrees, bool clockwise){
+	_clockwise = clockwise;
+	if(_mode == Stepper::HALF_STEP){
+		_steps = ((degrees * 4096)/360);
+	}
+	else{
+		_steps = ((degrees * 2048)/360);
+	}
+	return _sequence[_step];
+}
+
+
+uint8_t Stepper::next(){
+	if(!_steps){
+		PRINT_LOG(_logger, "[Stepper %d]  NO MAS PASOS\r\n",_id);
+		return _sequence[_step];
+	}
+    if(isOOL(_clockwise)){
+        _steps = 0;
+        PRINT_LOG(_logger, "[Stepper %d]  OUT OF RANGE\r\n",_id);
+		return _sequence[_step];
+    }
+	_steps--;
+	if(_clockwise){
+		_step = (_step < 7)? (_step+1) : 0;
+        _degrees += STEP_RESOLUTION;
+	}
+	else{
+		_step = (_step > 0)? (_step-1) : 7;
+        _degrees -= STEP_RESOLUTION;
+	}
+	PRINT_LOG(_logger, "[Stepper %d] Ang=%f  Quedan %d pasos\r\n", _id, _degrees, _steps);
+	return _sequence[_step];
+}
+
+
+bool Stepper::isOOL(bool clockwise, bool checklimit){
+    int16_t ideg = (int16_t)_degrees;
+	if(checklimit){
+		if(ideg < _min_deg_limit || ideg > _max_deg_limit){
+			return true;
+		}
+		return false;
+	}
+    if(clockwise && ((ideg + STEP_RESOLUTION) > _max_deg_limit)){
+        return true;
+    }
+    if(!clockwise && ((ideg - STEP_RESOLUTION) < _min_deg_limit)){
+        return true;
+    }
+    return false;
+}
---
+++ Stepper/Stepper.h
@@ -0,0 +1,134 @@
+/*
+    Copyright (c) 2017 @raulMrello
+ 
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+ 
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+ 
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+    
+    @file          Stepper.h
+    @purpose       Stepper motor controller
+    @version       v1.0
+    @date          Jun 2017
+    @author        @raulMrello
+*/
+
+#ifndef __STEPPER_H
+#define __STEPPER_H
+
+#include "mbed.h"
+#include "Logger.h"
+
+/**
+ * @author raulMrello
+ * @see API 
+ *
+ * <b>Stepper</b> Clase encargada de generar las siguientes activaciones de un motor paso a paso, basadas en una orden
+ * previamente solicitada. Las activaciones tendrán en cuenta el modo de trabajo del motor para saber el número de pasos
+ * a realizar, así como la configuración de los pines (wave_drive, full_step o half_step).
+ */
+class Stepper {
+public:
+	typedef enum{
+		WAVE_DRIVE,
+		FULL_STEP,
+		HALF_STEP
+	}Stepper_mode_t;
+    
+    /** Stepper()
+     *
+     * Constructor.
+     * Configura el modo de funcionamiento, por defecto FULL_STEP y asigna un identificador
+     * @param id Identificador
+     * @param mode	Modo de funcionamiento
+     * @param logger Objeto logger    
+     */
+	Stepper(uint8_t id, Stepper_mode_t mode = Stepper::FULL_STEP, Logger* logger = 0);
+
+    /** setRange()
+     *
+     * Establece el rango angular permitido
+     * @param maxdeg Límite positivo
+     * @param mindeg Límite negativo
+     */
+	void setRange(int16_t maxdeg, int16_t mindeg){_max_deg_limit = maxdeg; _min_deg_limit = mindeg;}
+    
+    /** ready()
+     *
+     * Obtiene el estado del driver, indicando si está listo para una nueva operación o si por lo
+     * contrario, está ocupado, realizando una pendiente.
+     * @return Estado actual [true|false] [listo|ocupado]
+     */
+	bool ready(){return ((_steps > 0)? false : true);}
+
+    /** request()
+     *
+     * Solicita un nuevo movimiento basado en un número de grados y un sentido concreto
+     * @param degrees Número de grados a girar
+     * @param clockwise Flag para indicar el sentido de giro
+     * @return Estado actual de las salidas a las que conectar el driver (x x x x IN1 IN2 IN3 IN4)
+     */
+	uint8_t request(uint16_t degrees, bool clockwise);
+
+    /** next()
+     *
+     * Obtiene la siguiente configuración de las salidas en la operación en curso. En caso de que
+     * la operación haya concluído, se obtendrá siempre el estado final.
+     * @return Estado actual de las salidas a las que conectar el driver (x x x x IN1 IN2 IN3 IN4)
+     */
+	uint8_t next();
+
+    /** getSteps()
+     *
+     * Obtiene el número de pasos que quedan para finalizar la orden actual
+     * @return Número de pasos pendientes
+     */
+	uint16_t getSteps(){return _steps;}
+    
+    /** getDegrees()
+     *
+     * Obtiene el ángulo de giro actual
+     * @return Número de pasos pendientes
+     */
+	float getDegrees(){return _degrees;}
+    
+    /** isOOL()
+     *
+     * Chequea si está fuera de rango en el siguiente movimiento
+	 * @param clockwise Sentido de giro
+	 * @param checklimit Chequea si está fuera de rango sin tener en cuenta el sentido
+	 *	de giro clockwise
+     * @return True si está fuera de rango.
+     */
+	bool isOOL(bool clockwise, bool checklimit = false);
+    
+protected:
+    static const int16_t MAX_DEG_DEFAULT = 90;
+	static const int16_t MIN_DEG_DEFAULT = 0;
+	uint8_t _id;
+	bool _clockwise;
+	uint16_t _steps;
+    float _degrees;
+    int16_t _max_deg_limit;
+    int16_t _min_deg_limit;
+	Stepper_mode_t _mode;
+	uint8_t _step;
+	const uint8_t * _sequence;
+    Logger* _logger;
+private:
+};
+
+#endif
